f7fe9916460b635f5618643dadee62bb8a89bc33
tiagohm <tiago.henrique.cco@gmail.com>
2020-09-11 09:29:36 -0300

    [android]: Migrar para a versão 1.12

--- android/app/src/main/kotlin/br/tiagohm/restler/MainActivity.kt
+++ android/app/src/main/kotlin/br/tiagohm/restler/MainActivity.kt
@@ -1,25 +1,5 @@
 package br.tiagohm.restler
 
-import android.os.Bundle
-import android.util.Log
-// import com.flurry.android.FlurryAgent
-// import com.flurry.android.FlurryPerformance
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-
-    /*
-    FlurryAgent.Builder()
-            .withCaptureUncaughtExceptions(true)
-            .withIncludeBackgroundSessionsInMetrics(true)
-            .withLogLevel(Log.VERBOSE)
-            .withPerformanceMetrics(FlurryPerformance.All)
-            .withLogEnabled(true)
-            .build(this, "4Y5XVXYKTB9GY33CB9BW")
-    */
-  }
-}
+class MainActivity: FlutterActivity()
